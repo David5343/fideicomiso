f7a5d59ac0dd80d742d597646aa3466692c2e358
Trabajo <david_5343@hotmail.com>
2024-02-27 16:03:03 -0600

    agregando controlador index a titulares y agregando rutas en clase api

---
+++ app/Http/Controllers/Api/Prestaciones/InsuredController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\Prestaciones;
+
+use App\Http\Controllers\Controller;
+use App\Models\Prestaciones\Insured;
+use Illuminate\Http\Request;
+
+class InsuredController extends Controller
+{
+    public function index()
+    {   
+        $titulares = Insured::where('status','active')
+                                ->latest()
+                                ->limit(25)
+                                ->get();
+        return response()->json($titulares);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Prestaciones\InsuredController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+    //Rutas de titulares
+    Route::get('api/prestaciones/titulares', [InsuredController::class,'index'])->name('prestaciones.titulares.index');
+    Route::get('api/prestaciones/titulares/create',[InsuredController::class, 'create'])->name('prestaciones.titulares.create');
+    Route::post('api/prestaciones/titulares',[InsuredController::class, 'store'])->name('prestaciones.titulares.store');
+    Route::get('api/prestaciones/titulares/{id}',[InsuredController::class, 'show'])->name('prestaciones.titulares.show');
+    Route::get('api/prestaciones/titulares/{id}/edit', [InsuredController::class,'edit'])->name('prestaciones.titulares.edit');
+    Route::put('api/prestaciones/titulares/{id}', [InsuredController::class,'update'])->name('prestaciones.titulares.update');
+    Route::get('api/prestaciones/titulares/{id}/disabled', [InsuredController::class,'disabled'])->name('prestaciones.titulares.disabled');
+    Route::put('api/prestaciones/titulares/baja/{id}', [InsuredController::class,'baja'])->name('prestaciones.titulares.baja');
