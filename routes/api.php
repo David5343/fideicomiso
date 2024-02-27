075d7c93556305b0b8eb57b9c6aa2c636aea3cf3
David <60202438+David5343@users.noreply.github.com>
2024-02-27 17:34:17 -0600

    actualizando rutas de api

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
     //Rutas de titulares
-    Route::get('api/prestaciones/titulares', [InsuredController::class,'index'])->name('prestaciones.titulares.index');
-    Route::get('api/prestaciones/titulares/create',[InsuredController::class, 'create'])->name('prestaciones.titulares.create');
-    Route::post('api/prestaciones/titulares',[InsuredController::class, 'store'])->name('prestaciones.titulares.store');
-    Route::get('api/prestaciones/titulares/{id}',[InsuredController::class, 'show'])->name('prestaciones.titulares.show');
-    Route::get('api/prestaciones/titulares/{id}/edit', [InsuredController::class,'edit'])->name('prestaciones.titulares.edit');
-    Route::put('api/prestaciones/titulares/{id}', [InsuredController::class,'update'])->name('prestaciones.titulares.update');
-    Route::get('api/prestaciones/titulares/{id}/disabled', [InsuredController::class,'disabled'])->name('prestaciones.titulares.disabled');
-    Route::put('api/prestaciones/titulares/baja/{id}', [InsuredController::class,'baja'])->name('prestaciones.titulares.baja');
+    Route::get('api/prestaciones/titulares', [InsuredController::class,'index']);
+    Route::get('api/prestaciones/titulares/create',[InsuredController::class, 'create']);
+    Route::post('api/prestaciones/titulares',[InsuredController::class, 'store']);
+    Route::get('api/prestaciones/titulares/{id}',[InsuredController::class, 'show']);
+    Route::get('api/prestaciones/titulares/{id}/edit', [InsuredController::class,'edit']);
+    Route::put('api/prestaciones/titulares/{id}', [InsuredController::class,'update']);
+    Route::get('api/prestaciones/titulares/{id}/disabled', [InsuredController::class,'disabled']);
+    Route::put('api/prestaciones/titulares/baja/{id}', [InsuredController::class,'baja']);
