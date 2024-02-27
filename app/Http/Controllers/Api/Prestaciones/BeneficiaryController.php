334e0badb4f25f809ad51d39c7ae5a666576e512
Trabajo <david_5343@hotmail.com>
2024-02-27 15:28:20 -0600

    empezando con la api del proyecto

---
+++ app/Http/Controllers/Api/Prestaciones/BeneficiaryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Prestaciones;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BeneficiaryController extends Controller
+{
+    //
+}
