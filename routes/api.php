b43acf579b6d66dae711fbc7cb14ca1aae098901
Trabajo <david_5343@hotmail.com>
2024-02-28 13:16:30 -0600

    revirtiendo cambios para  configuracion de api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\Prestaciones\InsuredController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Prestaciones\InsuredController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+Route::get('api/prestaciones/titulares', [InsuredController::class,'index']);
 Route::middleware('auth:sanctum')->group(function () {
     //Rutas de titulares
     Route::get('api/prestaciones/titulares', [InsuredController::class,'index']);
@@ -30,4 +31,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('api/prestaciones/titulares/{id}/disabled', [InsuredController::class,'disabled']);
     Route::put('api/prestaciones/titulares/baja/{id}', [InsuredController::class,'baja']);
 });
-Route::get('api/prestaciones/titulares', [InsuredController::class,'index']);
