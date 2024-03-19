2e40e3b685e145dd1ea60e041a014674c206ff9c
Trabajo <david_5343@hotmail.com>
2024-03-19 10:46:40 -0600

    definiendo parametros para hacer el login desde app Desktop

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -27,7 +27,7 @@ class ApiController extends Controller
             $user = User::where('email',$data->email)->first();
             if($user){
                 if(Hash::check($data->password,$user->password)){
-                    $token = $user->createToken($data->email);
+                    //$token = $user->createToken($data->email);
                     $response["status"] = 1;
                     //$response["token"] = $token->plainTextToken;
                     $response["msg"] = "Inicio de Session exitoso.";
