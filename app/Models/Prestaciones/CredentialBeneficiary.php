bfe2e4d1a74d653a09b52838f5f1424d4d05deb9
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2024-10-02 12:14:08 -0600

    agregando relacion de modelo credencialfamiliar con modelo familiar

--- app/Models/Prestaciones/CredentialBeneficiary.php
+++ app/Models/Prestaciones/CredentialBeneficiary.php
@@ -4,8 +4,14 @@ namespace App\Models\Prestaciones;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CredentialBeneficiary extends Model
 {
     use HasFactory;
+
+    public function beneficiary(): BelongsTo
+    {
+        return $this->belongsTo(Beneficiary::class);
+    }
 }
