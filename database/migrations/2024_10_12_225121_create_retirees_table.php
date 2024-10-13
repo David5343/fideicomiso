cdf4c438104df481e761ad042926a8d93ad6f4ae
David <60202438+David5343@users.noreply.github.com>
2024-10-13 10:47:03 -0600

    creando migracion para tabla retirees de pensionados

---
+++ database/migrations/2024_10_12_225121_create_retirees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('retirees', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_number',255)->nullable();
+            $table->date('start_date')->nullable();
+            $table->string('insured_type',255)->nullable();
+            $table->unsignedBigInteger('pension_type')->nullable();
+            $table->foreign('pension_type_id')->references('id')->on('pension_type');
+            $table->unsignedBigInteger('insured_id')->nullable();
+            $table->foreign('insured_id')->references('id')->on('insureds');
+            $table->unsignedBigInteger('beneficiary_id')->nullable();
+            $table->foreign('beneficiary_id')->references('id')->on('beneficiaries');
+            $table->string('pension_status',255)->nullable();
+            $table->enum('status', ['active', 'inactive','deleted']);
+            $table->string('modified_by',255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('retirees');
+    }
+};
