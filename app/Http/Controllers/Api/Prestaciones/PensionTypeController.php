4d63cb97418d7650518fabf009b72019c237fac8
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2024-10-22 15:28:31 -0600

    agregando metodo index de controlador para tipos de pensiones

--- app/Http/Controllers/Api/Prestaciones/PensionTypeController.php
+++ app/Http/Controllers/Api/Prestaciones/PensionTypeController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers\Api\Prestaciones;
 
 use App\Http\Controllers\Controller;
 use App\Models\Prestaciones\PensionType;
-use Illuminate\Http\Request;
 
 class PensionTypeController extends Controller
 {
     public function index()
     {
-        $tipoPensiones = PensionType::where('status','active')
+        $tipoPensiones = PensionType::where('status', 'active')
             ->get();
 
         return response()->json($tipoPensiones);
