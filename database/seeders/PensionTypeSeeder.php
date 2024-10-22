7dfacd7aba03d32a4f7fca3f41518bfd01e119ce
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2024-10-22 15:25:54 -0600

    agregando seeder de tipos de pensiones

--- database/seeders/PensionTypeSeeder.php
+++ database/seeders/PensionTypeSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Prestaciones\PensionType;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PensionTypeSeeder extends Seeder
