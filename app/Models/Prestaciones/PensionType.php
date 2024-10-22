db0f050b149a187f20a49343157697557eaee67a
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2024-10-22 15:27:43 -0600

    agregando relacion en tipo de pensiones con pensionados clase PensionType

--- app/Models/Prestaciones/PensionType.php
+++ app/Models/Prestaciones/PensionType.php
@@ -4,8 +4,14 @@ namespace App\Models\Prestaciones;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PensionType extends Model
 {
     use HasFactory;
+
+    public function retirees(): HasMany
+    {
+        return $this->hasMany(Retiree::class);
+    }
 }
